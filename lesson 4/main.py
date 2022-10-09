bedea240106435b1aa7db36a537eacdae9857c40
movavi_school <selleran@list.ru>
2022-10-09 13:13:26 +0700

    09.10.2022

---
+++ lesson 4/main.py
@@ -0,0 +1,32 @@
+# x='Строка'
+# x="Строка"
+# name="Антон\nАнтон\nАнтон"
+# name1='''
+# Строка1
+# Строка2
+# Строка3'''
+# print(name)
+# st="я большой"
+# print(st.upper())
+# st1="Я МАЛЕНЬКИЙ"
+# print(st1.lower())
+# st2="big small"
+# print(st2.capitalize())
+# familia=input("Фамилия:").capitalize()
+# ima=input("Имя:").capitalize()
+# otchestvo=input("Отчество:").capitalize()
+# print(familia, ima[0]+"."+otchestvo[0]+".")
+# print(f"{familia} {ima[0]}. {otchestvo[0]}.")
+# st=input("Введи слово/фразу")
+# print(st.count("а"))
+# print(st.lower().count
+# fraza=input("Введите слова через запятую: ")
+# lst=fraza.split(",")
+# lst.pop(0)
+# print(lst)
+# fraza=input("Введи строчку: ")
+# find=input("Что меняем: ")
+# replace=input("На что меняем: ")
+# print(fraza.replace(find,replace))
+fraza=input("Введи строчку: ")
+print(fraza.replace("ё","е"))
