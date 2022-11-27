606ba3d168b4104b20362d213547a6d3da3928af
movavi_school <klimnesterov8@gmail.com>
2022-11-27 13:13:23 +0700

    27 ноября

--- lesson 10/caesar.py
+++ lesson 10/caesar.py
@@ -1,4 +1,5 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+simvols=['(',')',',','.',' ','?','!']
 
 should_end=False
 while not should_end:
@@ -9,4 +10,20 @@ while not should_end:
     if shift> len(alphabet):
         shift=shift%len(alphabet)
     elif shift<-len(alphabet):
-        shift = shift % len(alphabet)
+        shift = shift % -len(alphabet)
+
+    cipher_text=''
+    for letter in text:
+        if letter in simvols:
+            cipher_text+=letter
+        else:
+            position=alphabet.index(letter)
+            if position+shift>len(alphabet):
+                new_pisition=position+shift-len(alphabet)
+            elif position + shift<0:
+                new_pisition = position + shift + len(alphabet)
+            else:
+                new_pisition = position + shift
+                cipher_text+=alphabet[new_pisition]
+    print(cipher_text)
+
