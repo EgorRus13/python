4319b343a03ce07944f7e06e8d791e6884ba5182
movavi_school <emnovitko@gmail..com>
2022-12-18 13:16:01 +0700

    18 декабря

---
+++ lesson 13/main.py
@@ -0,0 +1,48 @@
+# СЛОВАРИ
+# empty_dictionary={}
+# dictionary=dict()
+#
+# dict1={"Key":1,
+#        "Key2":"baldёzh",
+#        "Key3":[1,2,3],
+#        "Key4":{"Vladik":True}}
+# print(dict1["Key2"])
+# print(len(dict1))
+#
+#
+#
+#
+# МНОЖЕСТВА
+# 1)Повторения исключены
+# set1={"Anton","Anton","Anton"}
+# print(set1)
+# set2={"A","B","C"}
+# print(set2)
+# # tuple, float, int, str, bool
+# set3={1, "Odin", [1]}
+# # Error
+# empty_set=set()
+#
+#
+#
+#
+# ОПЕРАЦИИ СО МНОЖЕСТВАМИ
+# set1={1,2,3,4,5}
+# set2={3,4,5,6,7}
+# # Обьединения
+# print(set1.union(set2))
+# print(set1 | set2)
+# print(set1.intersection(set2))
+# print(set1.difference(set2))
+# print(set1.symmetric_difference(set2))
+# from random import randint
+# lst=[]
+# for _ in range(5):
+#     lst.append(randint(1,5))
+# print(lst)
+# unique=set(lst)
+# print(f"{len(unique)}шт.:{unique}")
+from random import randint
+size=randint()
+r_start=0
+r_end=10_000
