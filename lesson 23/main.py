46f392f897b7473148b90e324012023cfb5140f7
movavi_school <alexomger@mail.ru>
2023-03-12 13:10:39 +0700

    нет

---
+++ lesson 23/main.py
@@ -0,0 +1,25 @@
+# import json
+# a=open("file.json","w",encoding="utf-8")
+# content= json.dump("listik", a,ensure_ascii=False)
+# a=open("file.json","r",encoding="utf-8")
+# content= json.load("listik", a,ensure_ascii=False)
+# print(content)
+# a.close()
+
+
+# class Person:
+#     def __init__(self,imya):
+#         self.mishurik=imya
+#         self.status="Ychitel"
+#     def __str__(self):
+#         return f"{self.mishurik} {self.status}"
+#
+#     def common_method(self):
+#         print("сан ди ля ва ХЕЙ ХЕЙ ВАКА ВАКА ХЕЙ ХЕЙ")
+#
+# chelovek=Person("Ы")
+# print(chelovek.mishurik)
+# print(chelovek)
+# print(chelovek.common_method())
+#
+
