aba78388030b8cda923c9a35738460e4153065e6
movavi_school <lavr.@yandex.ru>
2023-04-23 13:16:52 +0700

    yes

--- lesson_29/main.py
+++ lesson_29/main.py
@@ -81,7 +81,92 @@ win=Tk()
 #
 # rb1=Radiobutton(win, text="Vitalik", font="Arial 15 normal", variable=rb_val, value=2, command=izmenenia)
 # rb1.pack()
+from tkinter import *
 
+win = Tk()
+win.geometry("500x500")
+
+# def click_rbm(event):
+#     ent.get()
+#     lab['text'] =ent.get()
+#
+#
+# lab = Label(win, text="////")
+# lab.pack()
+#
+#
+# ent = Entry(win)
+# ent.bind("<Button-3>", click_rbm)
+# ent.pack()
+
+
+# Задача №2
+# def izmeneniya():
+#     if rb_val.get() == 1:
+#         lab["text"] = start + " " + rb["text"]
+#     else:
+#         lab["text"] = start + " " + rb1["text"]
+#
+# rb_val = IntVar()
+# start = "Hello"
+# lab = Label(win, text=start + " ...", font="Arial 15 normal")
+# lab.pack()
+# rb = Radiobutton(win, text="Arkasha",
+#                  font="Arial 15 normal",
+#                  variable=rb_val,
+#                  value=1,
+#                  command=izmeneniya)
+# rb.pack()
+# rb1 = Radiobutton(win, text="Vitalik",
+#                   font="Arial 15 normal",
+#                   variable=rb_val,
+#                   value=2,
+#                   command=izmeneniya)
+# rb1.pack()
+#
+
+
+# Задача №3
+# def get_cb():
+#     print(cb_val.get())
+#     if cb_val.get() == True:# cb актив
+#         btn["state"] = "normal"
+#     else:
+#         btn["state"] = DISABLED
+#
+#
+# cb_val = BooleanVar()
+# cb = Checkbutton(win,
+#                 text="Актевировать",
+#                 variable=cb_val,
+#                 onvalue=True,
+#                 offvalue=False,
+#                 command=get_cb)
+# cb.pack()
+# # cb.select()# активация cb
+# # cb.deselect() # деактивация cb
+#
+#
+# btn = Button(win, text="Не актив!", state=DISABLED)
+# btn.pack()
+
+# задача №5
+colors = ('blue', 'red', 'black', 'orange', 'purple', 'yellow')
+
+
+def get_scala(val):
+    print(scala.get())  # значение либо так
+    print(val)  # либо так
+
+    win['bg'] = colors[scala.get()]
+
+
+scala = Scale(win, from_=0, to=5,
+              orient=HORIZONTAL,  # ориентация
+              length=300,  # длина в пикселях
+              command=get_scala
+              )
+scala.pack()
 
 
 
