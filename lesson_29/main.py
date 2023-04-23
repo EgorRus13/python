6df863254bfb3837ef98feed3c2200f7b4adc5fd
movavi_school <lavr.@yandex.ru>
2023-04-23 13:02:01 +0700

    yes

---
+++ lesson_29/main.py
@@ -0,0 +1,96 @@
+from tkinter import *
+win=Tk()
+# baton=Button(win, text="nigger")
+# baton.pack()
+# baton['text']="txt"
+# baton['height']=3
+# baton['width']=10
+# baton['font']="Arial 15 bold italic overstrike underline"
+# baton['bg']="orange"
+# baton['fg']="darkred"
+# baton['bd']=10
+
+
+
+
+
+
+# def get_rb():
+#     print(rb_val.get())
+#
+#
+# rb_val = IntVar()
+# rb1 = Radiobutton(win,
+#                   text="click me",
+#                variable=rb_val,
+#                value=470,
+#                command=get_rb)
+# rb1.pack()
+#
+#
+# rb2=Radiobutton(win,
+#                 text="no click me",
+#                variable=rb_val,
+#                value=2,
+#                command=get_rb)
+# rb2.pack()
+
+
+# def get_scala(val):
+#     print(scala.get())
+#
+# scala=Scale(win, from_=80, to=1000, orient=HORIZONTAL, length=300, tickinterval=3, resolution=2, command=get_scala)
+# scala.pack()
+
+
+
+
+
+# img=PhotoImage(file="scala.png")
+# img_big=img.subsample(2,2)
+# img_small=img.zoom(1,1)
+# lab=Label(win, image=img_big)
+# lab.pack()
+
+
+# Entry(win,bg="gold").pack()
+# Entry(win,bg="magenta").pack(ipady=10,ipadx=50,pady=30)
+
+
+
+# btn=Button(win, text="PEREKYS")
+# btn.pack()
+# btn['state']=DISABLED
+#
+# ent=Entry(win)
+# ent.pack()
+# ent.insert(0,"pi-pi py-py")
+# ent['state']="readonly"
+# def izmenenia():
+#     if rb_val.get() == 1:
+#         lab["text"]=start+" "+rb["text"]
+#     else:
+#         lab["text"]=start+" "+rb1["text"]
+# rb_val=IntVar()
+# start="Hello"
+# lab=Label(win,text=start + " ...", font="Arial 15 normal")
+# lab.pack()
+# rb=Radiobutton(win, text="Arkasha", font="Arial 15 normal", variable=rb_val, value=1, command=izmenenia)
+# rb.pack()
+#
+#
+# rb1=Radiobutton(win, text="Vitalik", font="Arial 15 normal", variable=rb_val, value=2, command=izmenenia)
+# rb1.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+win.mainloop()
